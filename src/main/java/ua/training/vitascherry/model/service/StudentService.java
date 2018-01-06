0ea4a4d246fe1e1c9b797fdda02ddaaa0b98152f
vitascherry <vitas.cherry@gmail.com>
2018-01-06 15:39:12 +0200

    Refactored

---
+++ src/main/java/ua/training/vitascherry/model/service/StudentService.java
@@ -0,0 +1,28 @@
+package ua.training.vitascherry.model.service;
+
+import ua.training.vitascherry.model.dao.DaoFactory;
+import ua.training.vitascherry.model.dao.UserDao;
+import ua.training.vitascherry.model.entity.User;
+
+import java.util.List;
+
+public class UserService {
+
+    private DaoFactory daoFactory;
+
+    public UserService(DaoFactory daoFactory) {
+        this.daoFactory = daoFactory;
+    }
+
+    public List<User> getAllStudents() {
+        try (UserDao dao = daoFactory.createUserDao()) {
+            return dao.findAll();
+        }
+    }
+
+    public User getStudentById(int id) {
+        try (UserDao dao = daoFactory.createUserDao()) {
+            return dao.findById(id);
+        }
+    }
+}
