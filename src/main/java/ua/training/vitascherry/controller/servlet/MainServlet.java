afcc96e455c295cab85121a678e234380e3e9be7
vitascherry <vitas.cherry@gmail.com>
2018-01-15 17:56:56 +0200

    Configured log4j2 to start with ServletContext. Config file placed to WEB-INF dir. Added Console and LogFile appenders

--- src/main/java/ua/training/vitascherry/controller/servlet/MainServlet.java
+++ src/main/java/ua/training/vitascherry/controller/servlet/MainServlet.java
@@ -1,5 +1,7 @@
 package ua.training.vitascherry.controller.servlet;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.training.vitascherry.controller.command.Command;
 import ua.training.vitascherry.controller.command.GetCommandMap;
 import ua.training.vitascherry.controller.command.PostCommandMap;
@@ -14,17 +16,25 @@ import static ua.training.vitascherry.controller.util.RequestMapper.extractComma
 
 public class MainServlet extends HttpServlet {
 
+    private static final Logger logger = LogManager.getLogger(MainServlet.class.getName());
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        logger.info("HTTP GET {}", req.getRequestURL());
         Command command = extractCommand(req, GetCommandMap.getInstance());
-        req.getRequestDispatcher(command.execute(req).getPage()).forward(req, resp);
+        String responsePage = command.execute(req).getPage();
+        logger.info("Response JSP page: {}", responsePage);
+        req.getRequestDispatcher(responsePage).forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        logger.info("HTTP POST {}", req.getRequestURL());
         Command command = extractCommand(req, PostCommandMap.getInstance());
-        req.getRequestDispatcher(command.execute(req).getPage()).forward(req, resp);
+        String responsePage = command.execute(req).getPage();
+        logger.info("Response JSP page: {}", responsePage);
+        req.getRequestDispatcher(responsePage).forward(req, resp);
     }
 }
